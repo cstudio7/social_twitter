994a3c56c5b590d1d18a7666bface0422ae8720c
Victor Godwin <victoranimate556@gmail.com>
2019-12-31 02:42:05 +0000

    new commit

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyDVJ6aK-d8zTsZzzINuePufU8husuYI52c",
+    authDomain: "teamwork-bbac8.firebaseapp.com",
+    databaseURL: "https://teamwork-bbac8.firebaseio.com",
+    projectId: "teamwork-bbac8",
+    storageBucket: "teamwork-bbac8.appspot.com",
+    messagingSenderId: "731106494423",
+    appId: "1:731106494423:web:a711aaa95e461d6586ad92",
+    measurementId: "G-6R1F7REGY3"
+};
